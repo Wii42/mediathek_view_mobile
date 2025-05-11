5be9e3312c6fa3e7d2f9687ff7376e160fbf2721
Wi42 <101128608+Wii42@users.noreply.github.com>
2025-05-11 15:56:39 +0200

    upgraded dependencies as far as possible

--- lib/widgets/introSlider/intro_slider.dart
+++ lib/widgets/introSlider/intro_slider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intro_slider/intro_slider.dart';
-//import 'package:intro_slider/slide_object.dart';
 
 class IntroScreen extends StatefulWidget {
   var onDonePressed;
