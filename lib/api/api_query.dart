be41e96cf742e5d13ac423b7295ba51eb46e7e9d
Wi42 <101128608+Wii42@users.noreply.github.com>
2025-06-01 18:39:24 +0200

    switch to use json.encode to create the api query

--- lib/api/api_query.dart
+++ lib/api/api_query.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 
@@ -17,14 +19,38 @@ class APIQuery {
   APIQuery({required this.onDataReceived, required this.onError});
 
   void search(String? genericQuery, SearchFilters searchFilters) {
-    List<String> queryFilters = [];
-
     logger.info("Query skip: $skip");
+
+    List<Map<String, dynamic>> queryFilters =
+        _buildSearchFilterList(searchFilters, genericQuery);
+
+    Map<String, dynamic> request = {
+      "queries": queryFilters,
+      "future": searchFilters.includeFutureVideos?.filterValue ?? false,
+      "sortBy": "timestamp",
+      "sortOrder": "desc",
+      "offset": skip,
+      "size": defaultQueryAmount
+    };
+
+    String requestString = json.encode(request);
+    logger.info("Firing request: $requestString");
+
+    execute(requestString);
+
+    skip = skip + defaultQueryAmount;
+  }
+
+  List<Map<String, dynamic>> _buildSearchFilterList(
+      SearchFilters searchFilters, String? genericQuery) {
+    List<Map<String, dynamic>> queryFilters = [];
 
     if (searchFilters.title != null &&
         searchFilters.title!.filterValue.isNotEmpty) {
-      queryFilters.add(
-          '{"fields":["title"],"query":"${searchFilters.title!.filterValue.toLowerCase()}"}');
+      queryFilters.add({
+        "fields": ["title"],
+        "query": searchFilters.title!.filterValue.toLowerCase()
+      });
     }
 
     if (searchFilters.topic != null &&
@@ -32,34 +58,34 @@ class APIQuery {
         genericQuery != null &&
         genericQuery.isNotEmpty) {
       //generics -> title only
-      queryFilters
-          .add('{"fields":["title"],"query":"${genericQuery.toLowerCase()}"}');
+      queryFilters.add({
+        "fields": ["title"],
+        "query": genericQuery.toLowerCase()
+      });
     } else if (genericQuery != null && genericQuery.isNotEmpty) {
-      queryFilters.add(
-          '{"fields":["topic","title"],"query":"${genericQuery.toLowerCase()}"}');
+      queryFilters.add({
+        "fields": ["topic", "title"],
+        "query": genericQuery.toLowerCase()
+      });
     }
 
     if (searchFilters.topic != null &&
         searchFilters.topic!.filterValue.isNotEmpty) {
-      queryFilters.add(
-          '{"fields":["topic"],"query":"${searchFilters.topic!.filterValue.toLowerCase()}"}');
+      queryFilters.add({
+        "fields": ["topic"],
+        "query": searchFilters.topic!.filterValue.toLowerCase()
+      });
     }
 
     if (searchFilters.channels != null) {
       for (var channel in searchFilters.channels!.filterValue) {
-        queryFilters
-            .add('{"fields":["channel"],"query":"${channel.toLowerCase()}"}');
+        queryFilters.add({
+          "fields": ["channel"],
+          "query": channel.toLowerCase()
+        });
       }
     }
-
-    String request =
-        '{"queries":[${queryFilters.join(',')}],"future":${searchFilters.includeFutureVideos?.filterValue ?? false},"sortBy":"timestamp","sortOrder":"desc","offset":$skip,"size":$defaultQueryAmount}';
-
-    logger.info("Firing request: $request");
-
-    execute(request);
-
-    skip = skip + defaultQueryAmount;
+    return queryFilters;
   }
 
   void resetSkip() {
