3f57feab687b179f3c961fdf16d240a999a915a6
Wi42 <101128608+Wii42@users.noreply.github.com>
2025-06-26 16:15:00 +0200

    add countly secrets template

---
+++ lib/util/countly_secrets.dart
@@ -0,0 +1,5 @@
+/// Make sure to not share your Countly app key and server URL publicly!
+class CountlySecrets {
+  static const String app = "your_countly_app_key_here";
+  static const String server = "https://your_countly_server_here";
+}
